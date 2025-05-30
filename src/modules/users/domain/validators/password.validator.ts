f0d6b3a057db65d8d51c6f7340f22ccabf084e6a
Marcus Miguel <marcusmigueell@gmail.com>
2025-05-29 23:58:25 -0300

    feat: :sparkles: refactor validators
    uuid validate incluse

--- src/modules/users/domain/validators/password.validator.ts
+++ src/modules/users/domain/validators/password.validator.ts
@@ -1,8 +1,10 @@
 import { UnprocessableEntityException } from "@nestjs/common"
 
 export class PasswordValidator {
-    static validate(password: string) {
-        if (!password || password.length < 8)
-            throw new UnprocessableEntityException("Senha deve ter no mínimo 8 caracteres")
+    static validate(password: string, up?: boolean): void {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/
+
+        if (!regex.test(password))
+            throw new UnprocessableEntityException(`A ${up ? "nova senha" : "senha"} deve ter no mínimo 8 caracteres, com pelo menos uma letra maiúscula, uma letra minúscula e um caractere especial`)
     }
 }
