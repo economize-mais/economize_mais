190373c5b6f851393798df6c93700582728efb1c
Marcus Miguel <marcusmigueell@gmail.com>
2025-11-01 18:46:15 -0300

    feat: :sparkles: maior de 18 anos
    regra para validar se o usuário tem mais de 18 anos

--- src/modules/users/application/dto/update-user.dto.ts
+++ src/modules/users/application/dto/update-user.dto.ts
@@ -1,25 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { Transform, Type } from "class-transformer"
 import {
     IsArray,
-    IsDateString,
+    IsDate,
     IsEmail,
     IsEnum,
+    IsNotEmpty,
     IsOptional,
     IsString,
     Matches,
     MaxLength,
     ValidateNested
 } from "class-validator"
-import { ApiProperty } from "@nestjs/swagger"
+
+import { startOfDayInTimezone } from "@/common/utils/date.utils"
 
 import { AddressDto } from "./address.dto"
+
 import { Gender } from "../../domain/enums/gender.enum"
 import { UserType } from "../../domain/enums/user-type.enum"
-import { Type } from "class-transformer"
 
 export class UpdateUserDto {
-
     id?: string
-    
+
     @ApiProperty({ example: "user@example.com" })
     @IsEmail()
     email: string
@@ -39,13 +42,18 @@ export class UpdateUserDto {
     @ApiProperty({ example: "(35)99942-1613", maxLength: 20 })
     @IsString()
     @IsOptional()
-    @MaxLength(20, { message: "O telefone deve ter no máximo 20 caracteres"})
+    @MaxLength(20, { message: "O telefone deve ter no máximo 20 caracteres" })
     phone?: string
 
-    @ApiProperty({ example: "1990-01-01", required: false })
+    @ApiProperty({
+        description: "Data de nascimento do usuário (YYYY-MM-DD)",
+        example: "1990-01-01"
+    })
+    @IsDate({ message: "startDate deve ser uma data válida" })
     @IsOptional()
-    @IsDateString()
-    birthDate?: string
+    @IsNotEmpty({ message: "A data de nascimento é obrigatória" })
+    @Transform(({ value }) => startOfDayInTimezone(value))
+    birthDate?: Date
 
     @ApiProperty({ enum: Gender, required: false })
     @IsOptional()
--- src/modules/users/domain/validators/user.validator.ts
+++ src/modules/users/domain/validators/user.validator.ts
@@ -1,21 +1,26 @@
 import { UnprocessableEntityException } from "@nestjs/common"
 
-import { CpfCnpjValidator } from "./cpf-cnpj.validator"
 import { CreateUserDto } from "../../application/dto/create-user.dto"
-import { PasswordValidator } from "./password.validator"
-import { RequiredFieldsValidator } from "./required-fields.validator"
 import { UpdateUserDto } from "../../application/dto/update-user.dto"
 
-export class UserValidator {
-    
-    public validate(data: CreateUserDto | UpdateUserDto): void {
-        if(!data)
-            throw new UnprocessableEntityException("Dados do usuário são obrigatórios")
+import { CpfCnpjValidator } from "./cpf-cnpj.validator"
+import { LegalAgeValidator } from "./legal-age.validator"
+import { PasswordValidator } from "./password.validator"
+import { RequiredFieldsValidator } from "./required-fields.validator"
 
-        if(data instanceof CreateUserDto)
+export class UserValidator {
+    public validate(data: CreateUserDto | UpdateUserDto): void {
+        if (!data)
+            throw new UnprocessableEntityException(
+                "Dados do usuário são obrigatórios"
+            )
+
+        if (data instanceof CreateUserDto)
             PasswordValidator.validate(data.password)
 
-        switch(data.userType) {
+        LegalAgeValidator.validate(data.birthDate)
+
+        switch (data.userType) {
             case "USER":
                 this.validateUserFields(data)
                 break
@@ -23,21 +28,27 @@ export class UserValidator {
                 this.validateCompanyFields(data)
                 break
             default:
-                throw new UnprocessableEntityException("Tipo de usuário inválido")
+                throw new UnprocessableEntityException(
+                    "Tipo de usuário inválido"
+                )
         }
     }
 
     private validateUserFields(data: CreateUserDto | UpdateUserDto): void {
         RequiredFieldsValidator.validate(data, ["fullName", "cpfCnpj"])
 
-        if(!CpfCnpjValidator.validate(data.cpfCnpj))
+        if (!CpfCnpjValidator.validate(data.cpfCnpj))
             throw new UnprocessableEntityException("CPF inválido")
     }
 
     private validateCompanyFields(data: CreateUserDto | UpdateUserDto): void {
-        RequiredFieldsValidator.validate(data, ["companyName", "tradeName", "cpfCnpj"])
+        RequiredFieldsValidator.validate(data, [
+            "companyName",
+            "tradeName",
+            "cpfCnpj"
+        ])
 
-        if(!CpfCnpjValidator.validate(data.cpfCnpj))
+        if (!CpfCnpjValidator.validate(data.cpfCnpj))
             throw new UnprocessableEntityException("CNPJ inválido")
     }
 }
